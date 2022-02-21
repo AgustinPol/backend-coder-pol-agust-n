d3d58a00daad1a2d4b53437f367ed9ee8548096d
Agustín Pol <agustinpol1991@hotmail.com>
2022-02-21 15:44:30 -0300

    modificando ultimos detalles antes de la entrega

--- 05/data/products.js
+++ 05/data/products.js
@@ -1,73 +1,21 @@
-const products = [
-    {
-      id: 1,
-      title: "Mouse Logitech rosa",
-      price: 2440,
-      thumbnail:
-      "https://http2.mlstatic.com/D_NQ_NP_2X_674801-MLA43269505256_082020-F.webp"
-  
-    },
-    {
-      id: 2,
-      title: "Mouse Logitech negro",
-      price: 1270,
-      thumbnail:
-        "https://http2.mlstatic.com/D_NQ_NP_2X_918568-MLA32146214305_092019-F.webp",
-    },
-    {
-      id: 3,
-      title: "Mouse Logitech Pebble gris",
-      price: 2499,
-      thumbnail:
-      "https://http2.mlstatic.com/D_NQ_NP_2X_735538-MLA43269505273_082020-F.webp"
-    },
-    {
-      id: 4,
-      title: "Teclado Gamer Redragon Dark Avenger QWERTY",
-      price: 4999,
-      thumbnail:
-      "https://http2.mlstatic.com/D_NQ_NP_2X_860827-MLA43976066837_112020-F.webp"
-    },
-    {
-      id: 5,
-      title: "Teclado Bluetooth Logitech QWERTY blanco",
-      price: 3957,
-      thumbnail:
-      "https://http2.mlstatic.com/D_NQ_NP_2X_893318-MLA46869134984_072021-F.webp"
-    },
-  ];
+class Products {
+  constructor() {
+    this.items = [];
+  }
 
-  class Products {
-    static lastProductId = products[products.length - 1].id;
+  save(product) {
+    const newItem = {
+      id: this.items.length + 1,
+      ...product,
+    };
+    this.items.push(newItem);
+    return newItem;
+  }
 
-    constructor() {
-        this.list = products;
-    }
+  getAll() {
+    return this.items;
+  }
 
-    getAll() {
-        return this.list;
-    }
-
-    getById(productId) {
-        return this.list.find(product => product.id === +productId);
-    }
-
-    save(product) {
-        const {title, price, thumbnail} = product;
-        if (!title || !price, !thumbnail) {
-            return null;
-        }
-        Products.lastProductId++;
-        const newProduct = {
-            id: Products.lastProductId,
-            title,
-            price,
-            thumbnail
-        }
-        this.list.push(newProduct);
-        return newProduct;
-    }
 }
-  
-  module.exports = Products;
-  
+
+module.exports = Products;
