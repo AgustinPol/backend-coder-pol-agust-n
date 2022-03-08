5acd36dc82c4933d46529ea68727174e7c335c7e
Agustín Pol <agustinpol1991@hotmail.com>
2022-03-08 10:27:03 -0300

    entrega desafio

--- primera-entrega-proyecto/src/app.js
+++ primera-entrega-proyecto/src/app.js
@@ -1,24 +1,27 @@
-const express = require('express');
-const productsRouter = require('./routes/products.js');
-const cartRouter = require('./routes/cart.js')
+const express = require("express");
+const productsRouter = require("./routes/products.js");
+const cartRouter = require("./routes/cart.js");
 
 const app = express();
 app.use(express.static("public"));
 
-app.use(express.urlencoded({
-    extended: true
-}));
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 app.use(express.json());
 
-app.use('/api/productos', productsRouter);
-app.use('/api/carrito', cartRouter);
-app.get('*', (req, res) => {
-    res.send({
-        status: "error",
-        error: `Could not find path ${req.url}`
-    })
-})
-
+app.use("/api/productos", productsRouter);
+app.use("/api/carrito", cartRouter);
+app.get("*", (req, res) => {
+  res.send({
+    status: "Error",
+    error: `Could not find path ${req.url}`,
+  });
+});
 
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => console.log(`Server running and listening on port ${PORT}`));
+app.listen(PORT, () =>
+  console.log(`Server running and listening on port ${PORT}`)
+);
--- primera-entrega-proyecto/src/routes/products.js
+++ primera-entrega-proyecto/src/routes/products.js
@@ -1,44 +1,44 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const ProductsHandler = require('../class/products');
+const ProductsHandler = require("../class/products");
 const apiProducts = new ProductsHandler();
-const authorizationMethod = require("../../admin/authorization")
+const authorizationMethod = require("../../admin/authorization");
 
+router.get("/(:id)?", (req, res) => {
+  const req_id = req.params["id"];
+  if (req_id) {
+    apiProducts.getProductById(req_id).then((result) => res.send(result));
+    return {
+      status: "success",
+      msg: "This is the product you were looking for",
+    };
+  }
+  apiProducts.getAllProducts().then((result) => res.send(result));
+  return {
+    status: "success",
+    msg: "This is the list of all products",
+  };
+});
 
-router.get('/(:id)?', (req, res) => {
-    const req_id = req.params['id'];
-    if(req_id){
-        apiProducts.getProductById(req_id).then(result=>res.send(result));
-        return{
-            status:"success",
-            msg: 'This is the product you were looking for'
-        }
-    }
-    apiProducts.getAllProducts().then(result => res.send(result));
-    return{
-        status:"success",
-        msg: "This is the list of all products"
-    }
-})
-
-router.post("/", authorizationMethod, (req,res)=>{
-    const body = req.body;
-    apiProducts.addProduct(body).then(result => res.send(result));
-})
+router.post("/", authorizationMethod, (req, res) => {
+  const body = req.body;
+  apiProducts.addProduct(body).then((result) => res.send(result));
+});
 
 router.put("/:id", authorizationMethod, (req, res) => {
-    let id = parseInt(req.params.id);
-    let product = req.body;
-    apiProducts.updateProduct(id, product).then((result) => res.send(result));
-})
+  let id = parseInt(req.params.id);
+  let product = req.body;
+  apiProducts.updateProduct(id, product).then((result) => res.send(result));
+});
 
 router.delete("/:id", authorizationMethod, (req, res) => {
-    const req_id = req.params["id"];
-    if (isNaN(req_id)) return res.send({
-        status: "error",
-        error: "Incorrect ID"
-    })
-    apiProducts.deleteById(req_id).then(product => res.send(product));
-})
-  
+  const req_id = req.params["id"];
+  if (isNaN(req_id))
+    return res.send({
+      status: "error",
+      error: "Incorrect ID",
+    });
+  apiProducts.deleteById(req_id).then((product) => res.send(product));
+});
+
 module.exports = router;
